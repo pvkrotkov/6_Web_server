59df270c4f9a5600520242a2fb004b5cb8172e24
Merc1337 <danimaers34@gmail.com>
2021-11-11 21:21:41 +0300

    code has been updated
    also i add html and ico files.

--- server.py
+++ server.py
@@ -1,31 +1,55 @@
 import socket
+from datetime import datetime
+sock = socket.socket() #создаём сокет
 
-sock = socket.socket()
+def locate(dec_inf,conn):
+    result=dec_inf.split('\n')[0].split()[1][1:] #вытаскиваем из запроса имя страницы
+    send_this=f'HTTP/1.1 200 OK\nServer: SelfMadeServer v0.0.1\nDate: {datetime.now().strftime("%a, %d %b %Y %H:%M:%S GTM")}\nContent-Type: text/html; \nConnection: close\n\n'
+    send_if_error=f'HTTP/1.1 400 ERROR \nServer:SelfMadeServer v0.0.1\nDate: {datetime.now().strftime("%a, %d %b %Y %H:%M:%S GTM")}\nContent-Type: text/html; \Connection: close \n\n'
+    if not result:
+        with open('index.html','r') as file:
+            info=file.read() #читаем файл
+            to_encode = send_this + info #конкатенируем
+            conn.send(to_encode.encode())#отправляем ответ
+    else:
+        try:
+            with open(result,'r') as file:
+                info=file.read()
+                to_encode=send_this+info
+                conn.send(to_encode.encode())
+        except FileNotFoundError:
+            with open('404.html','r') as err:
+                info=err.read()
+                to_encode=send_if_error+info
+                conn.send(to_encode.encode())
 
-try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
 
-sock.listen(5)
+def manipulator(conn):
+    inf=conn.recv(8192) #получаем запрос по 8кб
+    dec_inf=inf.decode() #переводим из битового представления
+    locate(dec_inf,conn)
 
-conn, addr = sock.accept()
-print("Connected", addr)
+def server_starter():
+    try:
+        sock.bind(('', 80)) #связываем сокет и порт
+        print("Using port 80")
+    except OSError:
+        sock.bind(('', 8080))
+        print("Using port 8080")
 
-data = conn.recv(8192)
-msg = data.decode()
+    sock.listen(5) #слушаем порт
+    while True:
+        try:
+            conn, addr = sock.accept() #принимаем соединение
+            print("Connected", addr)
+            manipulator(conn)
 
-print(msg)
+        except KeyboardInterrupt:
+            conn.close()
+            break
 
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
 
-Hello, webworld!"""
 
-conn.send(resp.encode())
 
-conn.close()
+if __name__=='__main__':
+    server_starter()

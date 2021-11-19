07867fd665ed7f1fe686d87bd91ff263f220b8a8
Alexa <70998859+AlexaErkb@users.noreply.github.com>
2021-11-19 15:56:24 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,31 +1,52 @@
 import socket
+import os
+from threading import Thread
+import datetime
+from os.path import exists, sep
 
-sock = socket.socket()
+def connection():
+    sock = socket.socket()
 
-try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
+    try:
+        sock.bind(('', 80))
+        print("Using port 80")
+    except OSError:
+        sock.bind(('', 8080))
+        print("Using port 8080")
 
-sock.listen(5)
+    sock.listen(5)
+    while True:
+        conn, addr = sock.accept()
+        print("Connected", addr)
+        Thread(target=run, args=[conn, addr[0]]).start()
 
-conn, addr = sock.accept()
-print("Connected", addr)
+def run(conn, addr):
+    dat = conn.recv(64000)
+    msg = dat.decode()
+    print(msg)
+    site = data(msg, addr)
+    conn.send(site)
 
-data = conn.recv(8192)
-msg = data.decode()
+def data(msg, addr):
+    try:
+        file = msg.split('\n')[0].split()[1][1:]
+    except:
+        file = "index.html"
+    if not file:
+        file = "index.html"
+    site = os.getcwd() + sep + file
+    if exists(site):
+        with open(site, "rb") as s:
+            ss = s.read()
+        return f"HTTP/1.1 200 OK\n" \
+                f"Date: {datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')}\n" \
+                f"Content-type: text/html\n" \
+                f"Server: SelfMadeServer v0.0.1\n" \
+                f"Content-length: {len(ss)}\n" \
+                f"Connection: close\n" \
+                f"\r\n\r\n".encode() + ss
+    else:
+        return ""
 
-print(msg)
 
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
-conn.send(resp.encode())
-
-conn.close()
+connection()

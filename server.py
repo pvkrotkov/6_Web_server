4ddc950f3a53bf7775bc2abd3d0561c8a31eb18d
sbourneiko <70803921+sbourneiko@users.noreply.github.com>
2021-11-21 22:12:57 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,61 @@
 import socket
+from datetime import datetime
+from pathlib import Path
+from wsgiref.handlers import format_date_time
+from threading import Thread
 
-sock = socket.socket()
 
-try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
+ROOT = 'www'
 
-sock.listen(5)
 
-conn, addr = sock.accept()
-print("Connected", addr)
+def get_timestamp():
+    return format_date_time(datetime.utcnow().timestamp())
 
-data = conn.recv(8192)
-msg = data.decode()
 
-print(msg)
+def get_body_length(body):
+    return len(body.encode())
 
-resp = """HTTP/1.1 200 OK
+
+def get_resource_path(request):
+    resource = request.split('\n')[0].split()[1][1:]
+    if not resource:
+        resource = 'index.html'
+    return Path(ROOT, resource)
+
+
+def get_response(body):
+    return f"""HTTP/1.1 200 OK
+Date: {get_timestamp()}
 Server: SelfMadeServer v0.0.1
-Content-type: text/html
+Content-Type: text/html
+Content-Length: {get_body_length(body)}
 Connection: close
+{body}"""
 
-Hello, webworld!"""
 
-conn.send(resp.encode())
+def handle(conn):
+    with conn:
+        request = conn.recv(8192).decode()
+        print(request)
+        resource_path = get_resource_path(request)
+        body = resource_path.read_text()
+        conn.send(get_response(body).encode())
 
-conn.close()
+
+def _main():
+    with socket.socket() as sock:
+        try:
+            sock.bind(('', 80))
+            print("Using port 80")
+        except OSError:
+            sock.bind(('', 8080))
+            print("Using port 8080")
+        sock.listen()
+        while True:
+            conn, addr = sock.accept()
+            print("Connected", addr)
+            Thread(target=handle, args=[conn]).start()
+
+
+if __name__ == '__main__':
+    _main()

e0b8a11d4bef389a5adfd66e7929923dbeef82e0
kate kirillova <76176114+gyul-i@users.noreply.github.com>
2021-11-21 23:34:59 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,38 @@
 import socket
+from datetime import datetime
+from threading import Thread
 
-sock = socket.socket()
+def vkl_serv():
+    sock = socket.socket()
+    try:
+        sock.bind(('', 80))
+        print("Using port 80")
+    except OSError:
+        sock.bind(('', 8080))
+        print("Using port 8080")
 
-try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
+    sock.listen(5)
+    while True:
+        conn, addr = sock.accept()
+        thread = Thread(target=veb, args=(conn, addr))
+        thread.start()
 
-sock.listen(5)
+def veb(conn, addr):
+    h = f'HTTP/1.1 200 OK\n\
+             Server: SelfMadeServer v0.0.1\n\
+             Date: {datetime.now()}\n\
+             Content-Type: text/html; charset=utf-8\n\
+             Connection: close\n\n'
+    er="""HTTP/1.1 404 
+    NOT FOUND"""
+    user = conn.recv(1024).decode()
+    path = user.split(" ")[1]
+    if path == '/':
+        with open('index.html', 'rb') as file:
+            ht = file.read()
+            conn.send(h.encode('utf-8') + ht)
+    else:
+        conn.send(er.encode('utf-8'))
 
-conn, addr = sock.accept()
-print("Connected", addr)
-
-data = conn.recv(8192)
-msg = data.decode()
-
-print(msg)
-
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
-conn.send(resp.encode())
-
-conn.close()
+if __name__ == "__main__":
+        vkl_serv()

ba6761cb7ebcf1d13f6f1cb39234d20b6ee6e220
arhipovaei <katya.arkhlpova3@gmail.com>
2021-11-22 04:29:40 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,37 @@
 import socket
+from threading import Thread
+from datetime import datetime
 
-sock = socket.socket()
+def server():
+    sock = socket.socket()
+    try:
+        sock.bind(('', 80))
+        print("Using port 80")
+    except OSError:
+        sock.bind(('', 8080))
+        print("Using port 8080")
+    sock.listen(5)
+    while True:
+        conn, addr = sock.accept()
+        print("Connected", addr)
+        thread = Thread(target=web_page, args=(conn, addr,))
+        thread.start()
 
-try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
+def web_page(conn, addr):
+    resp = f'HTTP/1.1 200 OK\n\
+        Server: SelfMadeServer v0.0.1\n\
+        Date: {datetime.now()}\n\
+        Content-Type: text/html\n\
+        Connection: close\n\n'
+    user = conn.recv(1024).decode()
+    path = user.split(" ")[1]
+    if path == '/' or path == '/index.html':
+        with open('index.html', 'rb') as file:
+            answer = file.read()
+            conn.send(resp.encode('utf-8') + answer)
+    else:
+        conn.send(("""HTTP/1.1 Page not found. Error: 404""").encode('utf-8'))
 
-sock.listen(5)
+if __name__ == "__main__":
+    server()
 
-conn, addr = sock.accept()
-print("Connected", addr)
-
-data = conn.recv(8192)
-msg = data.decode()
-
-print(msg)
-
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
-conn.send(resp.encode())
-
-conn.close()

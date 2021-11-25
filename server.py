c2524d555a30a83035d25bba7c78631964d83560
Андрей <51806719+666Jonah666@users.noreply.github.com>
2021-11-25 14:10:14 +0000

    Низкоуровневая работа с веб Никитин А.О. ПИ20-3
    Никитин Андрей Олегович ПИ20-3

--- server.py
+++ server.py
@@ -1,31 +1,19 @@
-import socket
+'''
+Перед запуском имеет смысл изменить self.path на свою директорию!
+'''
+import http.server
+import socketserver
+#my custom extended SimpleHTTPRequestHandler class with custom handler
+class HTTP_Server(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/media/sf/shared_folder': #set server directory. This is my directory!!!!
+            self.path = 'index.html' #set html file to display
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
-sock = socket.socket()
-
+http_server = socketserver.TCPServer(("", 80), HTTP_Server) #create tcp server
+#start server
 try:
-    sock.bind(('', 80))
-    print("Using port 80")
-except OSError:
-    sock.bind(('', 8080))
-    print("Using port 8080")
-
-sock.listen(5)
-
-conn, addr = sock.accept()
-print("Connected", addr)
-
-data = conn.recv(8192)
-msg = data.decode()
-
-print(msg)
-
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
-conn.send(resp.encode())
-
-conn.close()
+    http_server.serve_forever()
+except KeyboardInterrupt:
+    print("\nserver close")
+    http_server.server_close()

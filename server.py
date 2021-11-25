ae03175974f19bc9cd6f615bba1f07f4870a6a2d
Daniel <90207052+Daniel2727272727@users.noreply.github.com>
2021-11-25 14:39:35 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-
+import os
 sock = socket.socket()
 
 try:
@@ -10,22 +10,35 @@ except OSError:
     print("Using port 8080")
 
 sock.listen(5)
-
 conn, addr = sock.accept()
 print("Connected", addr)
-
 data = conn.recv(8192)
 msg = data.decode()
-
+    
 print(msg)
+n = msg.split()[1][1:]
+rasr = str(n).split('.')[1]
+print(n)
+print('--------------------')
+print(rasr)
+dost_rasr = ["js","html","css","php","jpg", "png", "gif", "ico"]
+if n in os.listdir():
+    file = n
+elif rasr in dost_rasr:
+    file = "404.html"
+else:
+    file = "403.html"
+    
+res = open(file)
+result = res.read()
+print("\nFile:\n", result)
 
 resp = """HTTP/1.1 200 OK
 Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
+        
+{0}""".format(result)
 
-Hello, webworld!"""
-
+    
 conn.send(resp.encode())
-
+    
 conn.close()

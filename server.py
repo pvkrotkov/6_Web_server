5b4a5f88a46ac31e6005038675259472b2c22fb9
Nikita! <89978326+kkhristich@users.noreply.github.com>
2021-12-17 17:34:49 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,38 @@
 import socket
+from wsgiref.handlers import format_date_time
+from datetime import datetime
+import threading
+
+
+PAGES = 'pages'
+
+
+def process_conn(conn):
+    data = conn.recv(8192)
+    msg = data.decode()
+
+    print(msg)
+
+    resource = PAGES + msg.split('\n')[0].split()[1]
+    if resource == PAGES + '/':
+        resource = resource + 'index.html'
+    with open(resource) as file:
+        content = file.read()    
+
+    date = format_date_time(datetime.utcnow().timestamp())
+
+    resp = f"""HTTP/1.1 200 OK
+Date: {date}
+Server: SelfMadeServer v0.0.1
+Content-type: text/html
+Content-length: {len(content.encode())}
+Connection: close
+{content}"""
+
+    conn.send(resp.encode())
+
+    conn.close()
+
 
 sock = socket.socket()
 
@@ -9,23 +43,11 @@ except OSError:
     sock.bind(('', 8080))
     print("Using port 8080")
 
-sock.listen(5)
+sock.listen()
 
-conn, addr = sock.accept()
-print("Connected", addr)
-
-data = conn.recv(8192)
-msg = data.decode()
-
-print(msg)
-
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
-conn.send(resp.encode())
-
-conn.close()
+while True:
+    conn, addr = sock.accept()
+    print("Connected", addr)
+    
+    threading.Thread(target=process_conn, args=[conn]).start()
+    
